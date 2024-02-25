01f64b3dba426b45e537b9a8df7d25a7e2d95b55
it2911 <cc8602@gmail.com>
2024-02-25 11:44:23 +0900

    update connector

--- dbgpt/datasource/rdbms/conn_tidb.py
+++ dbgpt/datasource/rdbms/conn_tidb.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from typing import Any, Iterable, Optional
+from urllib.parse import quote
+from urllib.parse import quote_plus as urlquote
 from dbgpt.datasource.rdbms.base import RDBMSDatabase
 
 
@@ -13,4 +16,19 @@ class TiDBConnect(RDBMSDatabase):
     db_dialect: str = "tidb"
     driver: str = "mysql+pymysql"
 
-    default_db = ["information_schema", "performance_schema", "sys", "tidb"]
+    @classmethod
+    def from_uri_db(
+        cls,
+        host: str,
+        port: int,
+        user: str,
+        pwd: str,
+        db_name: str,
+        engine_args: Optional[dict] = None,
+        **kwargs: Any,
+    ) -> RDBMSDatabase:
+        db_url: str = (
+            f"{cls.driver}://{quote(user)}:{urlquote(pwd)}@{host}:{str(port)}/{db_name}"
+            f"?ssl_ca=/etc/ssl/certs/ca-certificates.crt&ssl_verify_cert=true&ssl_verify_identity=true"
+        )
+        return cls.from_uri(db_url, engine_args, **kwargs)
